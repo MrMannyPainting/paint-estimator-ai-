faa92bb921c699e029994368fcf939fb46e2e8ad
MrMannyPainting <mannypainting09@gmail.com>
2025-05-20 21:06:29 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Paint Estimator AI is running!'
-
-if __name__ == '__main__':
-    app.run()
+def home():
+    return "Paint Estimator AI is running!"
