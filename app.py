c9f937ee9e56cee1100526256cceed8d3bf74df1
MrMannyPainting <mannypainting09@gmail.com>
2025-05-20 22:14:36 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return jsonify({"message": "Paint Estimator AI is running!"})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+def hello():
+    return "Paint Estimator AI is live!"
